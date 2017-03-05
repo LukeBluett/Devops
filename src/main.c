806c42ccd1afe4478b452a4af1f0b778de672377
Luke Bluett <LukeBluett@mail.com>
2017-03-05 16:03:29 +0000

    Connect C with glade
    modified:   res/login.glade
    modified:   src/main.c
    
    Added correct signals to login.glade file
    
    Got Glade file opening in C program.
    Login in button and cancel button connected.
    On destroy method moved above the main method to function correctly.

--- src/main.c
+++ src/main.c
@@ -1,22 +1,34 @@
-#include <stdio.h>
 #include <gtk/gtk.h>
+
+void button_login_button_press_event(GtkWidget *widget, gpointer data) {
+	g_print("login clicked\n");
+}
+
+void button_cancel_button_press_event(GtkWidget *widget, gpointer data) {
+	g_print("cancel clicked\n");
+}
+
+void on_window_login_destroy() {
+	gtk_main_quit();
+}
 
 int main(int argc, char *argv[]) {
 	// launch GUI
 	GtkBuilder *builder;
 	GtkWidget *window;
-
+	GtkWidget *buttonLogin;
 	gtk_init(&argc, &argv);
 
 	builder = gtk_builder_new();
 	gtk_builder_add_from_file(builder, "../res/login.glade", NULL);
 
 	window = GTK_WIDGET(gtk_builder_get_object(builder, "window_login"));
+	buttonLogin = GTK_WIDGET(gtk_builder_get_object(builder, "button_login"));
 	gtk_builder_connect_signals(builder, NULL);
 
 	g_object_unref(builder);
 
-	gtk_window_show(window);
+	gtk_widget_show(window);
 	gtk_main();
 
 	
@@ -26,6 +38,3 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-void on_window_main_destroy() {
-	gtk_main_quit();
-}
